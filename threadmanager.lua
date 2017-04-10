384a2e92aded83cf5e5787f925480157034afbb3
Christopher Moore <christopher.e.moore@gmail.com>
2017-04-10 15:25:04 +0100

    moved static assert resume function into ext.coroutine

--- threadmanager.lua
+++ threadmanager.lua
@@ -5,8 +5,9 @@ threads are added through add and updated through update
 
 local table = require 'ext.table'
 local class = require 'ext.class'
+local coroutine = require 'ext.coroutine'
 
-ThreadManager = class()
+local ThreadManager = class()
 
 function ThreadManager:init()
 	self.threads = table()
@@ -29,26 +30,12 @@ function ThreadManager:addMainLoopCall(f, ...)
 	self.mainLoopCalls:insert{f, ...}
 end
 
-function ThreadManager:updateThread(thread)
-	if coroutine.status(thread) == 'dead' then
-		return false
-	else
-		local res, err = coroutine.resume(thread)
-		if not res then
-			print(err)
-			print(debug.traceback(thread))
-			return false
-		end
-	end
-	return true
-end
-
 function ThreadManager:update()
 	-- update threads
 	local i = 1
 	while i <= #self.threads do
 		local thread = self.threads[i]
-		if not self:updateThread(thread) then
+		if not coroutine.assertresume(thread) then
 			self.threads:remove(i)
 		else
 			i = i + 1
